b1f7aeea9383c5cabd4647a637b5e52063537b38
Utkarshini <utkarshininarayan@gmail.com>
2023-07-30 17:45:36 +0530

    :art: Format code

--- server.js
+++ server.js
@@ -9,31 +9,37 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  let locDate = { temp: "Temp", desc: "Description", location: "Location", humidity: "Humidity ", feel: "Feel ", speed: "Speed" };
-    res.render("index", { locDate: locDate,});
+  let locDate = {
+    temp: "Temp",
+    desc: "Description",
+    location: "Location",
+    humidity: "Humidity ",
+    feel: "Feel ",
+    speed: "Speed",
+  };
+  res.render("index", { locDate: locDate });
 });
 
 app.post("/", async (req, res) => {
   try {
-      const location = await req.body.city;
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.APIKEY}&units=metric`;
-      let response = await fetch.then((module) => module.default(url));
-      let data = await response.json();
-      let locDate = {};
-      locDate.temp = Math.round(data.main.temp);
-      locDate.desc = data.weather[0].description;
-      locDate.feel = data.main.feels_like;
-      locDate.humidity = data.main.humidity;
-      locDate.speed = data.wind.speed;
-      locDate.location = location;
-      console.log(locDate);
-      res.render("index", { locDate: locDate,});
+    const location = await req.body.city;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.APIKEY}&units=metric`;
+    let response = await fetch.then((module) => module.default(url));
+    let data = await response.json();
+    let locDate = {};
+    locDate.temp = Math.round(data.main.temp);
+    locDate.desc = data.weather[0].description;
+    locDate.feel = data.main.feels_like;
+    locDate.humidity = data.main.humidity;
+    locDate.speed = data.wind.speed;
+    locDate.location = location;
+    console.log(locDate);
+    res.render("index", { locDate: locDate });
   } catch (err) {
-      console.log(err);
-      res.status(400).json({ data: 'not found!' })
+    console.log(err);
+    res.status(400).json({ data: "not found!" });
   }
 });
-
 
 const port = 3000;
 
